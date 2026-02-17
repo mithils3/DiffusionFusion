8c93c9e6885669c44462412e290ff7ef2c67cded
mithil <mithilsalunkhe@outlook.com>
2026-02-17 10:56:46 -0600

    Add print statement for labels in CustomDataset __getitem__ method

--- JiT/util/dataset.py
+++ JiT/util/dataset.py
@@ -14,7 +14,7 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         features = np.array(self.hf_dataset[idx]["feature"])
         labels = np.array(self.hf_dataset[idx]["label"])
-        
+        print(labels)
         # make sure the dims are features are 3d and labels are 1d
         assert features.ndim == 3, f"Expected features to be 3D, but got {features.ndim}D"
         assert labels.ndim == 1, f"Expected labels to be 1D, but got {labels.ndim}D"
