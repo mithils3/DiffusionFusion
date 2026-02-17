a5b28f7813ccfc6811ece640f4d228fdcc337081
mithil <mithilsalunkhe@outlook.com>
2026-02-17 10:57:31 -0600

    Refactor data type conversion in CustomDataset __getitem__ method to use torch tensors

--- JiT/util/dataset.py
+++ JiT/util/dataset.py
@@ -12,10 +12,10 @@ class CustomDataset(Dataset):
         return len(self.hf_dataset)
 
     def __getitem__(self, idx):
-        features = np.array(self.hf_dataset[idx]["feature"])
-        labels = np.array(self.hf_dataset[idx]["label"])
+        features = torch.tensor(self.hf_dataset[idx]["feature"])
+        labels = torch.tensor(self.hf_dataset[idx]["label"]).long()
         print(labels)
         # make sure the dims are features are 3d and labels are 1d
         assert features.ndim == 3, f"Expected features to be 3D, but got {features.ndim}D"
         assert labels.ndim == 1, f"Expected labels to be 1D, but got {labels.ndim}D"
-        return {"x": torch.from_numpy(features), "y": torch.from_numpy(labels).long()}
+        return {"x": features, "y": labels.long()}
