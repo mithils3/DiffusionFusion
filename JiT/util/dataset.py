9027a7212e5253221470d72d796a0fdfd0706a5d
mithil <mithilsalunkhe@outlook.com>
2026-02-17 10:59:16 -0600

    updates

--- JiT/util/dataset.py
+++ JiT/util/dataset.py
@@ -14,7 +14,6 @@ class CustomDataset(Dataset):
     def __getitem__(self, idx):
         features = torch.tensor(self.hf_dataset[idx]["feature"])
         labels = torch.tensor(self.hf_dataset[idx]["label"]).long()
-        print(labels)
         # make sure the dims are features are 3d and labels are 1d
         assert features.ndim == 3, f"Expected features to be 3D, but got {features.ndim}D"
         return {"x": features, "y": labels}
