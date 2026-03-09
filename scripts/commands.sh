c18eab52c9d5b45e2823dc7e7a6081b385004ddf
mithil <mithilsalunkhe@outlook.com>
2026-03-09 16:38:04 -0500

    updates

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,5 +1,5 @@
 srun -A bgnp-dtai-gh --time=02:00:00 --nodes=1 --ntasks-per-node=32 \
---partition=ghx4-interactive --gpus=2 --mem=256g --pty /bin/bash
+--partition=ghx4-interactive --gpus=4 --mem=256g --pty /bin/bash
 
 apptainer run --nv \
     --bind /projects \
