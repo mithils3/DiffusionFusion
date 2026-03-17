211a6de39b331c4a275566414963932859b4f01e
mithil <mithilsalunkhe@outlook.com>
2026-03-17 15:34:10 -0500

    updates

--- JiT/feature_extraction/vae.py
+++ JiT/feature_extraction/vae.py
@@ -1,13 +1,4 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-"""
-A minimal training script for DiT using PyTorch DDP.
-Reference - https://github.com/chuanyangjin/fast-DiT
-"""
 
 
 from diffusers.models import AutoencoderKL
